d5cb00cfc39525d957f275885fcc139bec7dc8eb
kdmstj <rkddmstj429@gmail.com>
2022-07-18 20:02:25 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,30 @@
+package hello.hellospring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //시스템이 저장하는 아이템
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
